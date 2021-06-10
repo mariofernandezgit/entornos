9855c1e90577e5fde8b1cc12c932ac1840b2141f
mario <mariofh32@gmail.com>
2021-06-10 18:43:30 +0200

    Constructor de Hexágono

---
+++ src/main/java/maestredam/Hexagono.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maestredam;
+
+/**
+ *
+ * @author CHOLY
+ */
+public class Hexagono extends PoligonosRegulares {
+
+    public Hexagono() {
+        super();
+    }
+
+    
+    
+    @Override
+    public int obtenerPerimetro() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int superficie() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    //Comentario inicial de la clase Hexágono
+}
